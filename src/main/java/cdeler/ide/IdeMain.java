05dc0c121ef0a959846426a4a33b1c698f363fac
cdeler <serj.krotov@gmail.com>
2019-07-23 11:18:55 +0300

    Project initialisation continue (added fatJar task, small rfg)

--- src/main/java/cdeler/ide/IdeMain.java
+++ src/main/java/cdeler/ide/IdeMain.java
@@ -1,20 +1,24 @@
 package cdeler.ide;
 
 
-import java.awt.EventQueue;
+import java.awt.*;
 
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
+import javax.swing.*;
 
 public class IdeMain extends JFrame {
     private static final String IDE_FRAME_NAME = "RubyPit";
 
-    public IdeMain() {
+    private IdeMain() {
+        initializeUI();
+    }
+
+    private void initializeUI() {
         setTitle(IDE_FRAME_NAME);
         setSize(800, 600);
         setLocationRelativeTo(null);
 
-        var webIcon = new ImageIcon("src/main/resources/icons8-ruby-programming-language-64.png");
+
+        var webIcon = new ImageIcon(getClass().getResource("/icons8-ruby-programming-language-64.png"));
         setIconImage(webIcon.getImage());
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
