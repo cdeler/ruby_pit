d6ece6a0b791cc0ece11ac2ae8d931e08301fe7a
cdeler <serj.krotov@gmail.com>
2019-07-26 00:44:36 +0300

    Started to implement LineNumberingTextArea

--- src/main/java/cdeler/ide/LineNumberingTextArea.java
+++ src/main/java/cdeler/ide/LineNumberingTextArea.java
@@ -3,11 +3,18 @@ package cdeler.ide;
 import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.Element;
+import javax.swing.text.Utilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cdeler.core.FontLoader;
 
 public class LineNumberingTextArea extends JTextArea {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LineNumberingTextArea.class);
+
     private JTextArea textArea;
 
     public LineNumberingTextArea(JTextArea textArea) {
@@ -23,16 +30,38 @@ public class LineNumberingTextArea extends JTextArea {
     }
 
     private String getLineNumbersText() {
+        LOGGER.debug("Trace getLineNumbersText()");
         int caretPosition = textArea.getDocument().getLength();
         Element root = textArea.getDocument().getDefaultRootElement();
         StringBuilder lineNumbersTextBuilder = new StringBuilder();
 
-        lineNumbersTextBuilder.append("1").append(System.lineSeparator());
+        LOGGER.info("Trace getColumns=" + textArea.getColumns() + " getLineCount=" + textArea.getLineCount());
+        if (textArea.getGraphics() != null) { // it's null before rendering
+            try {
+                int rowStartOffset = Utilities.getRowStart(textArea, 0);
+                int endOffset = textArea.getLineEndOffset(textArea.getLineCount() - 1);
 
-        for (int elementIndex = 2; elementIndex < root.getElementIndex(caretPosition) + 2; elementIndex++) {
-            lineNumbersTextBuilder.append(elementIndex).append(System.lineSeparator());
+                while (rowStartOffset <= endOffset) {
+                    String lineNumber = getTextLineNumber(rowStartOffset);
+                    lineNumbersTextBuilder.append(lineNumber).append(System.lineSeparator());
+                    rowStartOffset = Utilities.getRowEnd(textArea, rowStartOffset) + 1;
+                }
+            } catch (BadLocationException e) {
+                e.printStackTrace();
+            }
         }
-
         return lineNumbersTextBuilder.toString();
     }
+
+    private String getTextLineNumber(int rowStartOffset) {
+        Element root = textArea.getDocument().getDefaultRootElement();
+        int index = root.getElementIndex(rowStartOffset);
+        Element line = root.getElement(index);
+
+        if (line.getStartOffset() == rowStartOffset)
+            return String.valueOf(index + 1);
+        else
+            return "";
+    }
+
 }
