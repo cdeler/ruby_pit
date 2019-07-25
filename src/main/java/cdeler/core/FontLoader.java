d4fd8ce5d751a07eb48cfcb397460be48ed9b02b
cdeler <serj.krotov@gmail.com>
2019-07-25 13:46:38 +0300

    UI work. Added text area with line numbers and custom font

---
+++ src/main/java/cdeler/core/FontLoader.java
@@ -0,0 +1,29 @@
+package cdeler.core;
+
+import java.awt.*;
+import java.awt.font.TextAttribute;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FontLoader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FontLoader.class);
+
+    public static Font load(String fontName, int fontSize) {
+        try (InputStream is = FontLoader.class.getResourceAsStream("/fonts/" + fontName + ".ttf")) {
+            var font = Font.createFont(Font.TRUETYPE_FONT, is);
+            Map attributes = font.getAttributes();
+            attributes.put(TextAttribute.LIGATURES, TextAttribute.LIGATURES_ON);
+            font = font.deriveFont(attributes);
+            return font.deriveFont((float) fontSize);
+        } catch (FontFormatException | IOException e) {
+            LOGGER.error("Cannot load font " + fontName, e);
+        }
+
+        return new Font("Serif", Font.PLAIN, fontSize);
+
+    }
+}
--- src/main/java/cdeler/ide/Ide.java
+++ src/main/java/cdeler/ide/Ide.java
@@ -1,5 +1,7 @@
 package cdeler.ide;
 
+import java.awt.*;
+
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -7,14 +9,12 @@ import javax.swing.event.DocumentListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import cdeler.core.FontLoader;
+
 // created using https://stackoverflow.com/questions/36384683/highlighter-highlights-all-the-textarea-instead-of-a
 // -specific-word-and-its-occur
 public class Ide extends JFrame {
     private static final Logger LOGGER = LoggerFactory.getLogger(Ide.class);
-
-    private final JPanel jPanel1;
-    private final JScrollPane jScrollPane1;
-    private final JTextArea textArea;
 
     private int windowWidth;
     private int windowHeight;
@@ -27,15 +27,15 @@ public class Ide extends JFrame {
         this.windowHeight = windowHeight;
         this.iconPath = iconPath;
         this.windowTitle = windowTitle;
-        this.textArea = new JTextArea();
-        this.jPanel1 = new JPanel();
-        this.jScrollPane1 = new JScrollPane();
+
         initialize();
 
         LOGGER.info("Ide is initialized");
     }
 
     private void initialize() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         setTitle(windowTitle);
         setSize(windowWidth, windowHeight);
         setLocationRelativeTo(null);
@@ -43,59 +43,52 @@ public class Ide extends JFrame {
         var webIcon = new ImageIcon(getClass().getResource(iconPath));
         setIconImage(webIcon.getImage());
 
+        var textArea = createJTextArea();
+        var lineNumbers = new LineNumberingTextArea(textArea);
+
         textArea.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent documentEvent) {
-                LOGGER.info("insertUpdate");
+                LOGGER.debug("insertUpdate");
+                lineNumbers.updateLineNumbers();
             }
 
             @Override
             public void removeUpdate(DocumentEvent documentEvent) {
-                LOGGER.info("removeUpdate");
+                LOGGER.debug("removeUpdate");
+                lineNumbers.updateLineNumbers();
             }
 
             @Override
             public void changedUpdate(DocumentEvent documentEvent) {
-                LOGGER.info("changedUpdate");
+                LOGGER.debug("changedUpdate");
+                lineNumbers.updateLineNumbers();
             }
         });
 
-        textArea.setColumns(20);
-        textArea.setRows(5);
-        jScrollPane1.setViewportView(textArea);
+        var jScrollPane = new JScrollPane(textArea);
+        jScrollPane.setRowHeaderView(lineNumbers);
 
-        GroupLayout jPanel1Layout = new GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-                jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(jScrollPane1, GroupLayout.Alignment.TRAILING,
-                                GroupLayout.DEFAULT_SIZE, 417, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-                jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(jScrollPane1, GroupLayout.Alignment.TRAILING,
-                                GroupLayout.DEFAULT_SIZE, 292, Short.MAX_VALUE)
-        );
+        var panel = new JPanel(new BorderLayout());
+        panel.add(jScrollPane);
 
-        GroupLayout layout = new GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel1, GroupLayout.PREFERRED_SIZE,
-                                        GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel1, GroupLayout.PREFERRED_SIZE,
-                                        GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE))
-        );
+        add(panel);
 
         pack();
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        lineNumbers.updateLineNumbers();
     }
+
+    private JTextArea createJTextArea() {
+        JTextArea textArea = new JTextArea();
+        textArea.setColumns(80);
+        textArea.setRows(60);
+        textArea.setLineWrap(true);
+        textArea.setEditable(true);
+
+        textArea.setFont(FontLoader.load("iosevka-regular", 20));
+
+        return textArea;
+    }
+
 }
